a61bd7b0fe85a3de6978c8b90df86ded583d05f5
Jefferson <jefferson.toledo@fengbrasil.com.br>
2022-01-01 15:10:18 -0300

    finished works

--- js/script-google.js
+++ js/script-google.js
@@ -1,14 +1,28 @@
 function handleCredentialResponse(response) {
-  
+  const data = jwt_decode(response.credential)
+
+  fullName.textContent = data.name;
+  sub.textContent = data.sub;
+  given_name.textContent = data.given_name;
+  family_name.textContent = data.family_name;
+  email.textContent = data.email;
+  email_verified.textContent = data.email_verified ? 'Verificado' : 'Não verificado';
+  picture.setAttribute("src", data.picture) = data.picture;
 }
 window.onload = function () {
   google.accounts.id.initialize({
-    client_id: "618103155660-gn9rfm534rues9fck6q3vntnh2g3vjvv.apps.googleusercontent.com",
+    client_id: "YOUT_CLIENT_ID",
     callback: handleCredentialResponse
   });
   google.accounts.id.renderButton(
-    document.getElementById("buttonDiv"),
-    { theme: "outline", size: "large" }  // customization attributes
+    document.getElementById("buttonDiv"), { 
+      theme: "filled_blue", 
+      size: "large",
+      type: "standard",
+      shape: "pill",
+      text: "$ {button.text}",
+      logo_alignment: "left",
+    }  // customization attributes
   );
   google.accounts.id.prompt(); // also display the One Tap dialog
 }
